94680453e5780459acbcda349aa9bd812137c4b9
Lucas Seixas <seixas.lc@gmail.com>
2017-11-20 19:35:39 -0200

    Fix

--- particles.js
+++ particles.js
@@ -1,7 +1,11 @@
-/* -----------------------------------------
-  Particulas feitas por -
-  Vincent Garreau  - vincentgarreau.com
- ------------------------------------------- */
+/* -----------------------------------------------
+/* Author : Vincent Garreau  - vincentgarreau.com
+/* MIT license: http://opensource.org/licenses/MIT
+/* Demo / Generator : vincentgarreau.com/particles.js
+/* GitHub : github.com/VincentGarreau/particles.js
+/* How to use? : Check the GitHub README
+/* v2.0.0
+/* ----------------------------------------------- */
 
 var pJS = function(tag_id, params){
 
@@ -88,7 +92,7 @@ var pJS = function(tag_id, params){
       events: {
         onhover: {
           enable: true,
-          mode: 'repulse'
+          mode: 'grab'
         },
         onclick: {
           enable: true,
